65697bdfb4f5cef344a855ed392454440c606f64
Naeverdev <lukas.aeberhard@gmail.com>
2025-09-24 16:18:27 +0200

    fix port config

--- app.py
+++ app.py
@@ -35,6 +35,6 @@ def contact():
 
 
 if __name__ == '__main__':
-
-   import os
-   app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
